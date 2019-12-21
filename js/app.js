dd0d09beb31e0d02df40685d552026eaede28198
Cicek97 <cicekabdulla82@gmail.com>
2019-12-21 13:56:52 +0400

    1221

---
+++ js/app.js
@@ -0,0 +1,40 @@
+var next=document.getElementById("next")
+var prev=document.getElementById("prev")
+var slider=document.querySelector(".slider ul")
+var nicat=0
+
+var bar=document.querySelector(".bars")
+var rev=document.querySelector(".rev")
+rev.onclick=function(){
+    document.querySelector(".mainoverlay").classList.remove("overactive")
+}
+bar.onclick=function(){
+    document.querySelector(".mainoverlay").classList.add("overactive")
+}
+window.onload=function(){
+    setInterval(nextSlide,4000)
+    document.querySelector(".preloader").style="opacity:0;visibility:hidden;transition:4s"
+    document.querySelector(".preloader").remove()
+}
+function nextSlide(){
+
+    nicat++;
+    console.log(slider.querySelectorAll("li").length+":"+nicat)
+    if(nicat== slider.querySelectorAll("li").length){
+        nicat=0
+    }
+    var mes=-(nicat*800)
+    
+    slider.style.left=mes+"px"
+}
+next.onclick=nextSlide
+prev.onclick=function(){
+    nicat--;
+    console.log(slider.querySelectorAll("li").length+":"+nicat)
+    if(nicat==-1){
+        nicat=slider.querySelectorAll("li").length-1
+    }
+    var mes=-(nicat*800)
+    
+    slider.style.left=mes+"px"
+}
